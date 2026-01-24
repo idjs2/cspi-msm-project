2d908e27d84294720fdabd92a7b8e6f909fed8d7
idjs2 <idjs1102@naver.com>
2026-01-24 19:02:10 +0900

    clean

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import './App.css';
 
 function App() {
-  const env = process.env;
-  env.PUBLIC_URL = env.PUBLIC_URL || "";
-
   return (
     <div className="App">
-      <header>
-        <div className="right-bar">
-          <a href="https://accounts.google.com/b/0/AddMailService">Gmail</a>
-          <a href="https://www.google.com/imghp?hl=ko&tab=ri&authuser=0&ogbl">이미지</a>
-          <img src={process.env.PUBLIC_URL + `/assets/erlenmeyer-flask.png`}/>
-          <img src={process.env.PUBLIC_URL + `/assets/dots-menu.png`}/>
-        </div>
-      </header>
+      <h2>Hello World!</h2>
     </div>
   );
 }
